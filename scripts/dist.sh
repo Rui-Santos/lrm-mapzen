5ba63b1507002d9fe16a02e3630e390b86d1be42
Rui-Santos <rui.s@gmx.pt>
2018-03-16 18:36:56 +0000

    Update dist.sh

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -4,14 +4,14 @@ VERSION=`echo "console.log(require('./package.json').version)" | node`
 
 echo Building dist files for $VERSION...
 mkdir -p dist
-browserify -t browserify-shim src/*.js > dist/lrm-mapzen.js
-browserify -t uglifyify -t browserify-shim src/*.js | uglifyjs -c > dist/lrm-mapzen.min.js
+browserify -t browserify-shim src/*.js > dist/lrm-valhalla.js
+browserify -t uglifyify -t browserify-shim src/*.js | uglifyjs -c > dist/lrm-valhalla.min.js
 
-node-sass --output-style compressed -o dist/ src/styles/lrm-mapzen.scss
+node-sass --output-style compressed -o dist/ src/styles/lrm-valhalla.scss
 
-cp src/styles/lrm-mapzen-icons.svg dist/lrm-mapzen-icons.svg
-cp src/styles/lrm-mapzen-modes.svg dist/lrm-mapzen-modes.svg
-cp src/styles/lrm-mapzen-icons.png dist/lrm-mapzen-icons.png
+cp src/styles/lrm-valhalla-icons.svg dist/lrm-valhalla-icons.svg
+cp src/styles/lrm-valhalla-modes.svg dist/lrm-valhalla-modes.svg
+cp src/styles/lrm-valhalla-icons.png dist/lrm-valhalla-icons.png
 cp src/styles/routing-icon.png dist/routing-icon.png
 
 echo Done.
