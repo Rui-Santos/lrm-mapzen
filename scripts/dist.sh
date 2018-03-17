416f7dd6d74d5f0bf595fcda4d53ca6dfb031bc3
Rui-Santos <rui.s@gmx.pt>
2018-03-17 23:38:55 +0000

    Update dist.sh

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -4,14 +4,14 @@ VERSION=`echo "console.log(require('./package.json').version)" | node`
 
 echo Building dist files for $VERSION...
 mkdir -p dist
-browserify -t browserify-shim src/*.js > dist/lrm-valhalla.js
-browserify -t uglifyify -t browserify-shim src/*.js | uglifyjs -c > dist/lrm-valhalla.min.js
+browserify -t browserify-shim src/*.js > dist/tpp-lrm-valhalla.js
+browserify -t uglifyify -t browserify-shim src/*.js | uglifyjs -c > dist/tpp-lrm-valhalla.min.js
 
-node-sass --output-style compressed -o dist/ src/styles/lrm-valhalla.scss
+node-sass --output-style compressed -o dist/ src/styles/tpp-lrm-valhalla.scss
 
-cp src/styles/lrm-valhalla-icons.svg dist/lrm-valhalla-icons.svg
-cp src/styles/lrm-valhalla-modes.svg dist/lrm-valhalla-modes.svg
-cp src/styles/lrm-valhalla-icons.png dist/lrm-valhalla-icons.png
+cp src/styles/tpp-lrm-valhalla-icons.svg dist/tpp-lrm-valhalla-icons.svg
+cp src/styles/tpp-lrm-valhalla-modes.svg dist/tpp-lrm-valhalla-modes.svg
+cp src/styles/tpp-lrm-valhalla-icons.png dist/tpp-lrm-valhalla-icons.png
 cp src/styles/routing-icon.png dist/routing-icon.png
 
 echo Done.
